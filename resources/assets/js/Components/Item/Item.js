bd4e9a48af7c64f1e7cf8e45052c8db476226b59
EnesOG <sagir_enes@hotmail.com>
2018-10-29 23:39:32 +0100

    added stars to products

--- resources/assets/js/Components/Item/Item.js
+++ resources/assets/js/Components/Item/Item.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './Item.css';
 import {Link} from 'react-router-dom'
+import {FaStar} from 'react-icons/fa'
 import Aux from '../../Components/HOC/Aux';
 import Palet from '../../Assets/Images/pallet.jpg';
 
 
 const Item = (props) => {
 
+    const stars = (amount) =>{
+        let stars =[];
+     for(let i = 0; i < amount; i++) {
+       stars.push(<FaStar key={i}/>);
+    }
+    return stars;
+    };
 
     return(
 
@@ -14,7 +22,9 @@ const Item = (props) => {
             <div className="product-container">
                     <img src={Palet} alt="" className="product-image"/>
                       <p className="product-price">{props.dollar}{props.price}</p>
-                    <p className="product-stars"> {props.stars}</p>
+                    <div className='product-stars'>
+                    {stars(props.stars)}
+                    </div>
                     <p className="product-title">{props.children}</p>
                   </div>
         </Link>
--- resources/assets/js/Components/Products/Products.js
+++ resources/assets/js/Components/Products/Products.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import Aux from "../HOC/Aux";
 import Item from "../Item/Item";
-import { Link } from 'react-router-dom'
+import LazyLoad from 'react-lazyload'
+import './Products.css'
+import {Fade} from 'react-reveal'
 const Products = (props) =>{
   const ShowDollar = (isTrue) =>{
         if (isTrue === true){
@@ -14,16 +16,35 @@ const Products = (props) =>{
         return (
             <Aux>
                 <div className='container-fluid'>
+                    <div className="container">
+                    <div className="btn-group">
+                        <button className="btn btn-lg dropdown-toggle btnCustom" type="button"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Sort By
+                        </button>
+                        <div className="dropdown-menu">
+                            <a className="dropdown-item" href=" ">Top Rated</a>
+                            <a className="dropdown-item" href=" ">Price low to high</a>
+                            <a className="dropdown-item" href=" ">Price high to low</a>
+                        </div>
+                    </div>
+                    </div>
                     <div className="row">
                         {props.products.map(product =>
-
-                            <div className='col-xl-4 col-lg-4 col-md-6 col-sm-6 col-12' key={product.id}>
+                            <LazyLoad height={200} key={product.id}>
+                            <Fade up>
+                            <div className='col-xl-4 col-lg-4 col-md-6 col-sm-6 col-12'>
                                 <Item href={'/products/'+product.id+'/'+product.name.replace(/ +/g, "")}
+
+                                      stars={product.stars}
                                     price={product.price}
                                     dollar={ShowDollar(true)}>
                                     {product.name}
                                 </Item>
                             </div>
+
+                            </Fade>
+                            </LazyLoad>
                         )}
                     </div>
                 </div>
